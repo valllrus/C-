727cd78c2fe897de3c415a81b4d4d8f4019056a3
VOsipov <legoman380@mail.ru>
2023-07-19 12:37:44 +0300

    add Random Summ

--- Example_Summ/Program.cs
+++ Example_Summ/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 433;
-int numberB = 54775;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
